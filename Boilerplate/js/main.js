c9ad77c1ee892871cd9f0def440ecf78c66cd56e
judeharding <jharding@genesisgroupusa.com>
2018-02-05 15:02:16 -0500

    downloaded boilerplate

---
+++ Boilerplate/js/main.js
@@ -0,0 +1,12 @@
+
+// In the first few sections, we do all the coding here.
+// Later, you'll see how to organize your code into separate
+// files and modules.
+
+var Song = Backbone.Model.extend({
+    initialize: function(){
+        console.log("AS new song has been created.");
+    }
+});
+
+var song = new Song();
