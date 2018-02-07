0f3b9629d2281f84e89d677da0301ba38c7c856c
judeharding <jharding@genesisgroupusa.com>
2018-02-07 17:17:25 -0500

    views

--- notes.js
+++ notes.js
@@ -1,1 +1,20 @@
-collections are a list of models -- like tweets
+// Model Collection View
+//
+// Model variables have the first letter capitalized.  it is good to make it singular
+//    var Song = Backbone.Model.extend({
+//         initialize: function(){
+//             console.log("A new song has been created");
+//         }
+//     });
+//    it is a constructor function and you use the SET method to set attributes
+
+
+// Collection variables have the first letter capitalized.  it is good to make them plural
+//    var Songs = Backbone.Collection.extend({
+//        model: Song
+//    });
+//
+//
+// collections are a list of models -- like tweets
+
+views is not what you see -- it is the object that renders the content and changes
